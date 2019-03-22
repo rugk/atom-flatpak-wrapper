3b1ea0a56083252b3783487e9ac98f550b5d00d2
rugk <rugk+git@posteo.de>
2019-03-22 21:53:45 +0100

    Improve gpg script

--- flatpak-gpg.sh
+++ flatpak-gpg.sh
@@ -4,18 +4,23 @@
 # Atom flatpak. 
 #
 
-if [ -x gpg2 ]; then
-	GPG_PROGRAM="gpg2"
-else
-	GPG_PROGRAM="gpg"
-fi
-
+# first determinate whether we need to spawn anything to get outside of the flatpak
+flatpakSpawn=""
 # if this runs in a flatpak, spawn a command on the host instead
 if [ "$FLATPAK_ID" != "" ]; then
 	# --sandbox, --clear-env and --no-network do not work
-	flatpak-spawn --host $GPG_PROGRAM "$@"
+	flatpakSpawn="flatpak-spawn --host"
+	gpgProgram="$flatpakSpawn "
 else
 	# This is used when we sign a git commit outside of a flatpak
-	$GPG_PROGRAM "$@"
+	gpgProgram=""
 fi
 
+# finally spawn either gpg2 or gpg
+if $flatpakSpawn command -v gpg2 >/dev/null; then
+	gpgProgram="${gpgProgram}gpg2"
+else
+	gpgProgram="${gpgProgram}gpg"
+fi
+
+$gpgProgram "$@"
